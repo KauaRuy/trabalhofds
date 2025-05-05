b14634c9a7b2141618fb702ce94242b54b92210d
Kauã Ruy <kauaruy60@icloud.com>
2025-05-05 15:46:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const showMessageBtn = document.getElementById('botao');
+const messageElement = document.getElementById('mensagem');
+
+
+showMessageBtn.addEventListener('click', function() {
+   
+    messageElement.style.display = 'block';  
+    messageElement.textContent = 'Seja bem-vindo à minha página! Aqui falo um pouco de quem sou e gosto'; 
+});
